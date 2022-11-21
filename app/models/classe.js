5559854dfa89dddbc25ea5551b78d076d1682aa5
Jérôme Jossant <jerome.jossant@sourceamax.com>
2022-11-21 17:43:15 +0100

    add crud classe

---
+++ app/models/classe.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const classeSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        require: [true, 'Entrez un nom de classe'],
+        trim: true
+    }
+
+}, {
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: "updated_at"
+    }
+});
+
+module.exports = mongoose.model('Classe', classeSchema);
--- app/routes/classes.js
+++ app/routes/classes.js
@@ -1,55 +1,83 @@
-const {application, request, response} = require('express');
-const express = require('express');
-const {v4: uuidv4} = require('uuid');
+const express = require('express')
+//const { v4: uuidv4 } = require('uuid')
+const classeModel = require('../models/classe')
 
-let router = express.Router();
-let classes = [];
+let router = express.Router()
 
+router.post('/', async (request, response) =>{
+    const {label} = request.body
 
-router.post('/', (request, response) => {
+    if(typeof label === "undefined" || label == "") {
+        return response.status(500).json({
+            msg: "Veuillez saisir un label valide"
+        })
+    }
 
-    const {name} = request.body;
-
-    let classe = {
-        id: uuidv4(),
-        name
-    };
-
-    classes.push(classe)
-    response.status(200).json(classes);
-});
-
-router.get('/', (request, response) => {
-    response.status(200).json(classes);
-});
-
-router.get('/:id', (request, response) => {
-
-    const {id} = request.params
-
-    let classe = classes.find(o => o.id === id);
-    response.status(200).json(classe);
-    console.log(request.query, request.params);
-});
-
-router.delete('/:id', (request, response) => {
-    const {id} = request.params;
-
-    classes = classes.filter(Object => {
-        return Object.id !== id;
-    })
-    response.status(200).json(classes);
-});
-
-router.put('/:id', (request, response) => {
-    const {id} = request.params;
-    const {name} = request.body;
-
-    let classe = classes.find(o => o.id === id);
-
-    classe.name = name;
-
-    response.status(200).json(classe);
+    try{
+        let classe = await classeModel.create({
+            label
+        })
+        return response.status(200).json(classe)
+    }catch(error){
+        return response.status(500).json({
+            msg: error
+        })
+    }
 })
 
-module.exports = router;
+router.get('/', async (request, response) => {
+    try {
+        let classes = await classeModel.find()
+        return response.status(200).json(classes)
+    } catch (error) {
+        return response.status(500).json(error)
+    }
+})
+
+router.get('/:id', async (request, response) => {
+    const {id} = request.params
+
+    try {
+        let classe = await classeModel.findById(id)
+        return response.status(200).json(classe)
+    }catch (error) {
+        return response.status(500).json(error)
+    }
+
+
+})
+
+router.delete('/:id', async (request,response) => {
+    const {id} = request.params
+
+    try {
+        let classe = await classeModel.findByIdAndRemove(id)
+        return response.status(200).json({
+            msg: "Classe bien supprimée !"
+        })
+    }catch (error) {
+        return response.status(500).json(error)
+    }
+})
+
+router.put('/:id', async (request,response) =>{
+    const {id} = request.params
+    const {label} = request.body
+
+    try {
+        let classe = await classeModel.findByIdAndUpdate(id,
+            {
+                label
+            },{
+                new: true
+            })
+        return response.status(200).json({
+            msg: "Classe bien modifiée !"
+        })
+    }catch (error) {
+        return response.status(500).json(error)
+    }
+
+})
+
+module.exports = router
